039296cc2e3463d02f788ce9044bf465718f4f52
Na._.kyung <leena3823@gmail.com>
2023-10-13 20:43:36 +0900

    study

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,49 +1,29 @@
-import './App.css';
-import DiaryEditor from './DiaryEditor';
+import { useRef, useState } from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import React, {useRef,useState, useEffect } from "react";
-
-// const dummyList = [
-//   {
-//     id:1,
-//     author:"이나경",
-//     content:"하이 1",
-//     emotion:2,
-//     created_date: new Date().getTime()
-//   },
-//   {
-//     id:2,
-//     author:"이나",
-//     content:"하이 2",
-//     emotion:3,
-//     created_date: new Date().getTime()
-//   },
-//   {
-//     id:3,
-//     author:"이",
-//     content:"하이 3",
-//     emotion:5,
-//     created_date: new Date().getTime()
-//   },
-// ];
 
 function App() {
+  const [data, setData] = useState([]);
 
-  const [data,setData] = useState([]);
+  const dataId = useRef(0);
 
-  const dataId= useRef(0);
-
-  const onCreate = (author,content,emotion) =>{
-    const created_date= new Date().getTime();
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
     const newItem = {
-      author,content,emotion,created_date,
-      id:dataId.current
-    }
-    dataId.current+=1;
-    setData([,newItem,...data]);
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current
+    };
+
+    dataId.current += 1;
+
+    setData([newItem, ...data]);
   };
 
-  const onDelete = (targetId) => {
+  const onRemove = (targetId) => {
     const newDiaryList = data.filter(
       (it) => it.id !== targetId
     );
@@ -52,8 +32,8 @@ function App() {
 
   return (
     <div className="App">
-     <DiaryEditor onCreate={onCreate}/>
-     <DiaryList onDelete ={onDelete} diaryList={data}/>
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} onRemove={onRemove} />
     </div>
   );
 }
