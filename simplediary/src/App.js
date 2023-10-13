6d0932478ee7adbb90524c78cbdb71ba33c0a7c3
Na._.kyung <leena3823@gmail.com>
2023-10-14 00:13:35 +0900

    23-10-14 study

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,12 +1,36 @@
-import { useRef, useState } from "react";
+import { useRef, useEffect, useState, useMemo } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+import OptimizeTest from "./OptimizeTest";
+
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async()=>{
+    const res = await fetch(`https://jsonplaceholder.typicode.com/comments`).then((res)=>res.json());
+    
+
+    const initData =res.slice(0,20).map((it)=>{
+      return{
+        author: it.email,
+        content:it.body,
+        emotion:Math.floor(Math.random()*5)+1,
+        created_date: new Date().getTime(),
+        id:dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(()=>{
+    getData();
+  },[]);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -30,10 +54,36 @@ function App() {
     setData(newDiaryList);
   };
 
+  const onEdit = (targetid,newContent)=>{
+    setData(
+      data.map((it)=> //map 내장함수 이용해서 새로운 배열을 만들어서 setdata에 전달
+      it.id === targetid ? {...it, content:newContent}: it
+      )
+    );
+  };
+
+  const getDiaryAnalysis= useMemo(
+    ()=>{
+    
+
+    const goodCount=data.filter((it)=>it.emotion>=3).length;
+    const badCount=data.length-goodCount;
+    const goodRatio=(goodCount/data.length)*100;
+    return{goodCount,badCount,goodRatio};
+  },[data.length]
+  );
+
+  const{goodCount,badCount,goodRatio}=getDiaryAnalysis;//값으로 사용해야함 함수랑 똑같은 역할 
+
   return (
     <div className="App">
+      <OptimizeTest/>
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} onRemove={onRemove} />
+      <div>전체 일기: {data.length}</div>
+      <div>기분 좋은 일기 갯수: {goodCount}</div>
+      <div>기분 나쁜 일기 갯수: {badCount}</div>
+      <div>기분 좋은 일기 비율: {goodRatio}</div>
+      <DiaryList onEdit={onEdit} diaryList={data} onRemove={onRemove} />
     </div>
   );
 }
--- simplediary/src/DiaryItem.js
+++ simplediary/src/DiaryItem.js
@@ -1,11 +1,46 @@
+import {useRef, useState} from 'react';
+// react hooks
 const DiaryItem = ({
-  onDelete,
+  onRemove,
   id,
   author,
+  onEdit,
   content,
   emotion,
   created_date
 }) => {
+
+  const [isEdit, setIsEdit]=useState(false); //true 와 false boolean 형태를 갖게된다.
+  const toggleIsEdit =()=>setIsEdit(!isEdit); //토글 반전연산 isedit이 true이면 수정중이라고 간주해서 수정 폼 띄움. 
+
+  const [localContent, setLocalContent]=useState(content);
+  const localContentInput = useRef();
+
+
+  const handleRemove =() => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  }
+
+  const handleEdit = () =>{
+
+    if(localContent.length < 5){ //길이가 5 이하이면 수정 불가능
+      localContentInput.current.focus();
+      return;
+    }
+
+    if(window.confirm(`${id}번 째 일기를 수정하시겠습니까? `)){
+      onEdit(id,localContent);
+      toggleIsEdit();
+    }
+    
+  }
+
+  const handleQuitEdit=()=>{
+    setIsEdit(false);
+    setLocalContent(content);
+  }
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -15,16 +50,27 @@ const DiaryItem = ({
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
-      <div className="content">{content}</div>
-      <button
-        onClick={() => {
-          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
-            onDelete(id);
-          }
-        }}
-      >
-        삭제하기
-      </button>
+      <div className="content">
+      {isEdit ? (
+      <>
+      <textarea ref= {localContentInput} value={localContent} onChange={(e)=> setLocalContent(e.target.value)} />
+      </>
+      ):(
+      <>{content}</>
+    )}
+    </div>
+
+    {isEdit ? (
+      <>
+    <button onClick={handleQuitEdit}>수정 취소</button>
+    <button onClick={handleEdit}>수정 완료</button>
+    </>
+    ):(
+      <>
+      <button onClick={handleRemove}>삭제하기</button>
+      <button onClick={toggleIsEdit}>수정하기</button>
+      </>
+    )}
     </div>
   );
 };
