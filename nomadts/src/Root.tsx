8bcd6fe73258268301d78ce79c8c0bae9c4d1552
Na._.kyung <leena3823@gmail.com>
2023-10-26 15:47:20 +0900

    nomad createbrowser

---
+++ nomadts/src/Root.tsx
@@ -0,0 +1,10 @@
+
+function Root(){
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default Root;
--- nomadts/src/index.tsx
+++ nomadts/src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import {ThemeProvider} from "styled-components";
-import { darkTheme, lighttheme } from './theme';
+import { RouterProvider } from 'react-router-dom';
+import Router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-    <App />
-    </ThemeProvider>
+    
+    <RouterProvider router = {Router}/>
+    
   </React.StrictMode>
 );
 
