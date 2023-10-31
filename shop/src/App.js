29e0b742b5c039a387afcaef8a396118ee5d8257
Na._.kyung <leena3823@gmail.com>
2023-10-31 13:43:46 +0900

    23-10-31 react study

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import { Navbar, Container, Nav} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useState} from "react";
+import { useState} from "react";
 import data from './data.js';
 import Card from './Card';
 import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom';
 import Detail from './routes/Detail';
+import axios from "axios";
+import Cart from './routes/Cart';
+
+
 
 function App() {
 
-  let [shoes]=useState(data)
+  let [shoes, setShoes]=useState(data)
+  let[재고]=useState([10,11,12])
+
   let navigate = useNavigate();
-  
+  let [button, setButton]=useState(2);
+
   return (
     <div className="App">
 
@@ -21,33 +28,41 @@ function App() {
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
           <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
-            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail/:id')}}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
 
       <Routes>
-        <Route path="/" element={
-          <>
-          <div className='main-bg'></div>
-          <div className="container">
-            <div className="row">
-              {
-                shoes.map((a,i)=>{
-                  return (
-                    <Card shoes={shoes[i]} i={i}></Card>
-                  )
-                })
-              }
-            </div>
+    <Route path="/" element={
+      <>
+        <div className='main-bg'></div>
+        <div className="container">
+          <div className="row">
+            {
+              shoes.map((a,i)=>{
+                return (
+                  <Card shoes={shoes[i]} i={i} key={i}></Card>
+                )
+              })
+            }
           </div>
-          </>
-        }/>
-        <Route path="/detail/:id" element={<Detail shoes={shoes}/>}/>
-       
-
-      </Routes>
+        </div>
+        <button onClick={()=>{
+          axios.get('https://codingapple1.github.io/shop/data3.json').then((result)=>{
+            console.log(result.data);
+            let copy= [...shoes, ...result.data];
+            setShoes(copy);
+            setButton(button=>button+1);
+          }); 
+          axios.post('/sdfsdfs', {name: "na"});         
+        }}>버튼</button>
+      </>
+    }/>
+    <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
+    <Route path="/cart" element={<Cart/>} />
+</Routes>
 
     </div>
   );
--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,8 +1,11 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import {Nav} from 'react-bootstrap';
+
 
 function Detail(props){
 
+    
     useEffect(()=>{
         let a = setTimeout(()=>{setAlert(false) }, 2000)
         return ()=>{
@@ -11,7 +14,8 @@ function Detail(props){
     },[])
    
     let [alert,setAlert]=useState(true);
-    let[count,setCount]=useState(0);
+    
+    let [탭,탭변경]=useState(0);
 
     let {id} = useParams();
     id=parseInt(id);
@@ -24,8 +28,7 @@ function Detail(props){
                 2초 이내 구매시 할인 
                 </div>:null
             }
-            {count}
-            <button onClick={()=>{setCount(count+1)}}>버튼</button>
+            
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" alt="Shoe Image"/>
@@ -45,8 +48,40 @@ function Detail(props){
                     }
                 </div>
             </div>
+            <Nav variant="tabs"  defaultActiveKey="link0">
+    <Nav.Item>
+      <Nav.Link onClick={()=>{탭변경(0)}} eventKey="link0">버튼0</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link onClick={()=>{탭변경(1)}}eventKey="link1">버튼1</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link onClick={()=>{탭변경(2)}} eventKey="link2">버튼2</Nav.Link>
+    </Nav.Item>
+</Nav>
+<TabContent shoes={props.shoes} 탭={탭}/>
+
         </div>
     )
 }
+
+function TabContent({ 탭 , shoes}){
+
+    let[fade,setFade]=useState('')
+    
+
+    useEffect(()=>{
+        let a=setTimeout(()=>{setFade('end')}, 100)
+        
+        return ()=>{
+            clearTimeout(a)
+            setFade('')
+        }
+    },[탭])
+    return <div className={'start' + fade}>
+        {[<div>내용0</div>,<div>내용1</div>,<div>내용2</div>][탭]}
+    </div>
+}
+
 
 export default Detail;
