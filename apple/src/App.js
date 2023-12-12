da382a1fc88bef50419136c63e91a27299cf4fc7
Na._.kyung <leena3823@gmail.com>
2023-12-12 09:33:10 +0900

    23-12-12 study

--- apple/src/App.js
+++ apple/src/App.js
@@ -3,45 +3,19 @@ import { useState } from 'react';
 
 function App() {
 
-  let [글제목,글제목변경]=useState(['남자 코트 추천', '여자 코드 추천', '잠실 우동 맛집']);
-  let [따봉,따봉변경] = useState(0);
+  let post= '강남 우동 맛집'
+  let [글제목,b] = useState('남자 코트 추천');
 
-  function 함수(){
-    console.log(1);
-  }
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color:'red', fontSize:'15px'}}>블로그입니다</h4>
-      </div>
-
-      
-      <button onClick={()=>{
-        
-        let copy=[...글제목];
-        copy.sort();
-        글제목변경(copy);
-      }}>가나다순정렬</button>
-
-      <div className="list">
-        <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
-        <p>2월 17일 발행</p>
+        <h4>블로그임</h4>
       </div>
       <div className="list">
-        <h4>{글제목[1]}</h4>
+        <h4>{글제목}</h4>
         <p>2월 17일 발행</p>
       </div>
-      <div className="list">
-        <h4>{글제목[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <div className='modal'>
-        <h4>제목</h4>
-        <p>날짜</p>
-        <ㅔ
-      </div>
-    </div>
+     </div>
   );
 }
 
