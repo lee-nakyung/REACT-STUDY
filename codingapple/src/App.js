3109d6f0b3ddcdc1f6708e1711625e41913ac517
Na._.kyung <leena3823@gmail.com>
2024-01-03 18:35:24 +0900

    23-01-03

--- codingapple/src/App.js
+++ codingapple/src/App.js
@@ -4,84 +4,24 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  let [글제목, 글제목변경] = useState(['ㄴㅇㄹ','나 오늘 생일', '생일 다음날 ', '생일 다다음날']);
-  let [따봉, 따봉변경] = useState(0);
-  let [modal, setModal] = useState(false);
-  let [title, setTitle]=useState(0);
-  let [입력값, 입력값변경] = useState('');
 
+  let post= "강남 우동 맛집";
+  let[title,setTitle]=useState('남자 코트 추천');
 
-  return (
-    <div className="App">
-      <div className="black-nav"></div>
-
-      {
-        글제목.map(function (a, i) {
-          return (
-            <div className="list" key={i}>
-              <h4 onClick={() => { setModal(true); setTitle(i)}}>{글제목[i]}<span onClick={() => { 따봉변경(따봉 + 1); }}>👍</span>{따봉}</h4>
-              <p>2월 17일 발행</p>
-              <button onClick={()=>{
-                let copy=[...글제목];
-                copy.splice(i,1);
-                글제목변경(copy);
-              }}>삭제</button>
-              <hr></hr>
-            </div>
-          );
-        })
-      }
-
-      <input onChange={(e)=> { 
-        입력값변경(e.target.value); 
-        }}/>
-      <button onClick={()=>{
-        let copy=[...글제목];
-        copy.unshift(입력값);
-        글제목변경(copy)
-      }}>글발행</button>
-
-      {
-        modal === true ? <Modal title={title} 글제목변경={글제목변경} 글제목={글제목}/> : null
-      }
-
-      <Modal2/>
-    </div>
-  );
-}
-
-
-
-
-function Modal(props) {
   
-  return (
-    <div className="modal">
-      <h4>{props.글제목[props.title]}</h4>
-      <p>날짜</p>
-      <p>상세내용</p>
-      <button>글수정</button>
+
+
+return (
+  <div className="App">
+    <div class="black-nav">
+      <h4>블로그임</h4>
     </div>
-  );
+    <div className="list">
+      <h4>{a}</h4>
+      <p>2월 17일 발행</p>
+      <hr/>
+    </div>
+  </div>
+)
 }
-
-class Modal2 extends React.Component{
-  constructor(){
-    super();
-    this.state={
-      name:'kim',
-      age:20
-    }
-  }
-  render(){
-    return (
-      <div>안녕 {this.state.age}
-      <button onClick={()=>{
-        this.setState({age:21})
-      }}>버튼</button>
-      </div>
-    )
-  }
-}
-
 export default App;
