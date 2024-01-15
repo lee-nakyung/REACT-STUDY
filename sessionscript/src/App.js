29e315f5079509db2a3ed073c3319c952e438134
Na._.kyung <leena3823@gmail.com>
2024-01-15 11:00:30 +0900

    24-01-15

--- sessionscript/src/App.js
+++ sessionscript/src/App.js
@@ -3,17 +3,34 @@ import './App.css';
 import { useState } from "react";
 
 function App() {
-  let [like, setLike] = useState(0);
+  const [isOpen, setOpen] = useState(false);
+
+  const openModal=()=>{
+    setOpen(true);
+  }
+
+  const closeModal=()=>{
+    setOpen(false);
+  }
+  return (
+    <div>
+      <button onClick={openModal}>모달 열기</button>
+      <Modal isOpen={isOpen} onClose={closeModal} />
+    </div>
+  );
+}
+
+function Modal({ isOpen, onClose }) {
+  if (!isOpen) return null; //false 이면 아무것도 렌더링 하지 않음
 
   return (
     <>
-      <p>{like}</p>
-    </>
+      <h2>모달입니다.</h2>
+      <p>아기사자 여러분 안녕하세요.</p>
+      <button onClick={onClose}>닫기</button>
+     </>
   );
-
 }
-
-
 
 
 export default App;
