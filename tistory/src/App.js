82fd555721bebca018dec223ada5bf68ebe24bbe
Na._.kyung <leena3823@gmail.com>
2024-01-17 10:07:13 +0900

    24-01-17

---
+++ tistory/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+import Info from './Info';
+
+function App() {
+
+  const [visible, setVisible]=useState(false);
+
+  return (
+    <div>
+     <button onClick={()=>{
+      setVisible(!visible);
+     }}>
+      {visible ? '숨기기' : '보이기'}
+     </button>
+
+     <hr/>
+     {visible && <Info/>}
+    </div>
+  );
+}
+
+export default App;
