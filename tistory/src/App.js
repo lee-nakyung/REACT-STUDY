b91fca69634f7c6fa78a560a76dcfd42e6cf39f0
Na._.kyung <leena3823@gmail.com>
2024-01-18 14:52:46 +0900

    24-01-18

--- tistory/src/App.js
+++ tistory/src/App.js
@@ -1,69 +1,13 @@
-import styled from "styled-components";
-import "./App.css";
-import ToDoButton from "./ToDoButton"
-import { useState } from "react";
-import { useEffect } from "react";
+import React from 'react';
 
-function App() {
-  let Btn = styled.button`
-    margin-top: 2rem;
-    border: none;
-    width: 5rem;
-    background: ${(props) => props.bg};
-    cursor: pointer;
-  `;
-
-  const [list, setList] = useState([]);
-  const [userInput, setUserInput] = useState("");
-
-  useEffect(() => {
-    console.log(userInput); 
-  }, [userInput]); 
-
-  function todoContent(event) {
-    setUserInput(event.target.value);
-  }
-
-  function addList() {
-    if (userInput.trim() === "") {
-      // 사용자가 아무 것도 입력하지 않았으면 아무것도 하지 않음
-      return;
-    }
-  
-    setList((prevList) => [...prevList, userInput]);
-    setUserInput(""); // 항목을 추가한 후 입력 필드 초기화
-  }
-  
-
-  function deleteList(index) {
-    setList((prevList) => {
-      return prevList.filter((_, i) => i !== index);
-    });
-  }
-  
+function App(){
+  const name="뤼액트";
 
   return (
-    <div className="App">
-      <div className="buttonBox">
-        <Btn bg="#FF9F9F">Add</Btn>
-        <Btn bg="aliceblue">Save</Btn>
-      </div>
-      <button className="plusButton" onClick={addList}>
-        Plus
-      </button>
-      {list.map((item, index) => {
-        return (
-          <ToDoButton
-            key={index}
-            title={item}
-            index={index}
-            addContent={todoContent}
-            delete={() => deleteList(index)}
-          />
-        );
-      })}
+    <div>
+      {name=='리액트' ? <h1>리액트입니다.</h1>: null};
     </div>
-  );
+  )
 }
 
 export default App;
