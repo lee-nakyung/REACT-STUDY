bc7d1e6351840dae89473fbd89f65a909e78a39e
이나경 <lnk5324@hansung.ac.kr>
2024-01-22 15:40:13 +0900

    24-01-22

---
+++ newsapi/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+import NewsList from './components/NewsList';
+
+const App = () => {
+  return <NewsList />;
+};
+
+export default App;
